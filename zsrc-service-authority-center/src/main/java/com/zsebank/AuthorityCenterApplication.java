fbc8254d6c142944c1f6793832ebbfbddac61b60
jianjiawen <michael_jjw@163.com>
2023-04-14 22:39:04 +0800

    20230414

---
+++ zsrc-service-authority-center/src/main/java/com/zsebank/AuthorityCenterApplication.java
@@ -0,0 +1,20 @@
+package com.zsebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 授权中心启动入口
+ * */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuthorityCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityCenterApplication.class,args);
+    }
+
+}
+
+
---
+++ zsrc-service-authority-center/src/main/java/com/zsebank/conf/Knife4jConfig.java
@@ -0,0 +1,33 @@
+package com.zsebank.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Knife4jConfig {
+    @Bean(value = "authority-center")
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .description("# Authority Center RESTful APIs")
+                        .termsOfServiceUrl("http://www.jjw.com/")
+                        .contact(new Contact("jianjiawen","http://www.jjw.com","93108@qq.com"))
+                        .version("v1.0")
+                        .build())
+                //分组名称
+                .groupName("v1.0")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.zsebank.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
