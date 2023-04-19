afd7190a5a8110791cde57cd9a2b05dca6e2ef8b
jjwangel <michael_jjw@163.com>
2023-04-19 18:00:02 +0800

    20230419

--- zsrc-service-gateway/src/main/java/com/zsebank/filter/HeaderTokenGatewayFilter.java
+++ zsrc-service-gateway/src/main/java/com/zsebank/filter/HeaderTokenGatewayFilter.java
@@ -1,25 +1,32 @@
 package com.zsebank.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /**
  * HTTP 请求头部携带 Token 验证过滤器
  * */
+@Slf4j
 public class HeaderTokenGatewayFilter implements GatewayFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         // 从 HTTP Header 中寻找 key 为 token, value 为 imooc 的键值对
         String name = exchange.getRequest().getHeaders().getFirst("token");
+        MultiValueMap<String,String> queryParams = exchange.getRequest().getQueryParams();
+
+        log.info("request query params:[{}]",queryParams.getFirst("serviceId"));
         if ("imooc".equals(name)) {
             return chain.filter(exchange);
         }
 
+        log.error("request not find token");
         // 标记此次请求没有权限, 并结束这次请求
         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
         return exchange.getResponse().setComplete();
--- zsrc-service-nacos-client/src/main/java/com/zsebank/controller/NacosClientController.java
+++ zsrc-service-nacos-client/src/main/java/com/zsebank/controller/NacosClientController.java
@@ -1,12 +1,10 @@
 package com.zsebank.controller;
 
+import com.alibaba.fastjson2.JSON;
 import com.zsebank.service.NacosClientService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +22,14 @@ public class NacosClientController {
     @GetMapping("/service-instance")
     public List<ServiceInstance> logNacosClientInfo(
             @RequestParam(defaultValue = "nacos-client") String serviceId) {
-        log.info("coming in log nacos client info: [{}]", serviceId);
+        log.info("logNacosClientInfo: [{}]", serviceId);
         return nacosClientService.getNacosClientInfo(serviceId);
+    }
+
+    @PostMapping("/service-instance2")
+    public List<ServiceInstance> logNacosClientInfo2(@RequestBody String serviceId) {
+        log.info("logNacosClientInfo2: [{}]", JSON.parseObject(serviceId).get("serviceId").toString());
+        return nacosClientService.getNacosClientInfo(JSON.parseObject(serviceId).get("serviceId").toString());
     }
 
 
